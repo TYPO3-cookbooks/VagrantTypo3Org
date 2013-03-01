733ad0904df1269833f1d4f812a81b1207b77f57
Christian Zenker <christian.zenker@599media.de>
2013-03-01 18:43:06 +0100

    [BUGFIX] solr installation package was Not Found
    netcologne seems to host the latest versions only
    
    Change-Id: I377cd556b4c2f1e7ab9442f57a8abf15bdeebdc8
    Reviewed-on: https://review.typo3.org/18712
    Reviewed-by: Steffen Gebert
    Tested-by: Steffen Gebert

--- site-cookbooks/solr/attributes/default.rb
+++ site-cookbooks/solr/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:solr][:version]   = "3.6.1"
-default[:solr][:link]      = "http://mirror.netcologne.de/apache.org/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
+default[:solr][:link] = "http://archive.apache.org/dist/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
 default[:solr][:checksum]  = "1b4552ba95c8456d4fbd596e82028eaa0619b6942786e98e1c4c31258543c708" #sha265
 default[:solr][:home]          = "/usr/share/solr"
 
