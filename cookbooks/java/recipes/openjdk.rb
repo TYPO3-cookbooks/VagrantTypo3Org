5029744bb5baaf9add5cd931b99749e4668ca717
Christian Zenker <typo3@xopn.de>
2013-08-29 13:20:32 +0200

    [BUGFIX] update java cookbook to 1.13.0 to compensate new Java packages in Debian
    Change-Id: Id8efd99ce901d795fe1daffbfaa781d99b2bf789
    Reviewed-on: https://review.typo3.org/23451
    Reviewed-by: Christian Zenker
    Tested-by: Christian Zenker

--- cookbooks/java/recipes/openjdk.rb
+++ cookbooks/java/recipes/openjdk.rb
@@ -1,9 +1,11 @@
-#
+# Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
 # Author:: Seth Chisamore (<schisamo@opscode.com>)
+# Author:: Joshua Timberman (<joshua@opscode.com>)
+#
 # Cookbook Name:: java
 # Recipe:: openjdk
 #
-# Copyright 2010-2011, Opscode, Inc.
+# Copyright 2010-2013, Opscode, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,23 +19,35 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-pkgs = value_for_platform(
-  ["centos","redhat","fedora"] => {
-    "default" => ["java-1.6.0-openjdk","java-1.6.0-openjdk-devel"]
-  },
-  "default" => ["openjdk-6-jdk","default-jdk"]
-)
+java_location = Opscode::OpenJDK.new(node).java_location
 
-execute "update-java-alternatives" do
-  command "update-java-alternatives -s java-6-openjdk"
-  returns [0,2]
-  action :nothing
-  only_if { platform?("ubuntu", "debian") }
+include_recipe 'java::set_java_home'
+
+if platform_family?('debian', 'rhel', 'fedora')
+
+  bash 'update-java-alternatives' do
+    code <<-EOH.gsub(/^\s+/, '')
+      update-alternatives --install /usr/bin/java java #{java_location} 1061 && \
+      update-alternatives --set java #{java_location}
+    EOH
+    action :nothing
+  end
+
 end
 
-pkgs.each do |pkg|
+if platform_requires_license_acceptance?
+  file "/opt/local/.dlj_license_accepted" do
+    owner "root"
+    group "root"
+    mode "0400"
+    action :create
+    only_if { node['java']['accept_license_agreement'] }
+  end
+end
+
+node['java']['openjdk_packages'].each do |pkg|
   package pkg do
     action :install
-    notifies :run, "execute[update-java-alternatives]"
+    notifies :run, 'bash[update-java-alternatives]', :immediately if platform_family?('debian', 'rhel', 'fedora')
   end
 end
