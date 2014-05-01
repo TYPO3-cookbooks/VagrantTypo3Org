c6aab742c7e62671b37bc6745b3447352c4a6294
Christian Zenker <typo3@xopn.de>
2014-05-02 00:18:20 +0200

    fix that composer can not be installed due to a missing setting
    Change-Id: I27295a77eb38976ff55be947396237c082870e72
    Reviewed-on: https://review.typo3.org/29767
    Reviewed-by: Christian Zenker
    Tested-by: Christian Zenker

--- site-cookbooks/dev/attributes/default.rb
+++ site-cookbooks/dev/attributes/default.rb
@@ -5,3 +5,6 @@ default[:dev][:xdebug][:remote][:host] = '10.0.2.2'
 default[:dev][:xdebug][:remote][:port] = 9000
 default[:dev][:xdebug][:remote][:idekey] = 'PHPSTORM'
 
+# setting needed for composer
+include_attribute 'php'
+normal['php']['directives']['suhosin.executor.include.whitelist'] = 'phar'
