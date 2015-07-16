ec5be844e6617d843e5a17460b125de3c76abde7
Michael Stucki <michael.stucki@typo3.org>
2015-07-16 22:09:49 +0200

    [TASK][t3org] Depend on database + hostsfile cookbooks

--- site-cookbooks/t3org/metadata.rb
+++ site-cookbooks/t3org/metadata.rb
@@ -8,3 +8,6 @@ recipe           "typo3", "Main recipe to provide TYPO3 and a database"
 recipe           "typo3::source", "Download TYPO3 sources from git repository"
 recipe           "typo3::tools", "Download packages which come in handy with TYPO3"
 recipe           "typo3::introduction", "Download introduction packages which come in handy with TYPO3"
+
+depends "database"
+depends "hostsfile"
